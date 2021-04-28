4020c0920a145fb36bf8340d47894f54e3e095cf
Davi <48662592+d-mageek@users.noreply.github.com>
2021-04-28 10:27:28 -0300

    Integration with Quick Menu

--- AVPreview3.0/Main.cs
+++ AVPreview3.0/Main.cs
@@ -1,9 +1,12 @@
-﻿using Harmony;
+﻿using System.Linq;
+using System.Reflection;
+using UnhollowerRuntimeLib;
+using Harmony;
 using MelonLoader;
 using UnityEngine;
 using VRC.SDK3.Avatars.Components;
-using System.Reflection;
-using UnhollowerRuntimeLib.XrefScans;
+using VRC.UI;
+using AVPreview.Utils;
 
 namespace AVPreview
 {
@@ -17,75 +20,66 @@ namespace AVPreview
     public class Main : MelonMod
     {
         private static MelonMod Instance;
-        public static HarmonyInstance HarmonyInstance => Instance.Harmony;
-
+        private static ActionMenuDriver actionMenu;
         private static AvatarPlayableController CurrentController;
-        private static bool Patch = true;
+        private static bool Patch;
 
         public override void OnApplicationStart()
         {
             Instance = this;
-            Instance.Harmony.Patch(typeof(ActionMenuDriver).GetMethod(nameof(ActionMenuDriver.Method_Public_AvatarPlayableController_0)), new HarmonyMethod(typeof(Main).GetMethod(nameof(Prefix))), new HarmonyMethod(typeof(Main).GetMethod(nameof(Postfix))));
+            ClassInjector.RegisterTypeInIl2Cpp<EnableDisableListener>();
+            HarmonyPatches();
         }
 
-        // To use: AVPreview.Main.XRef(nameof(<insert class>.<insert method>));
-        public static void XRef(string Name) { foreach (var instance in XrefScanner.UsedBy(typeof(AvatarPlayableController).GetMethod(Name))) if (instance.TryResolve() != null) MelonLogger.Msg(instance.TryResolve().FullDescription()); }
-
-        public static void Prefix(MethodBase __originalMethod) { if (Patch) PatchMenu(); }
-
-        public static void Postfix(MethodBase __originalMethod) { if (Patch) PatchMenu(); }
-
-        private static void PatchMenu()
+        public override void VRChat_OnUiManagerInit()
         {
-            MelonLogger.Msg("Patching ActionMenu...");
-            foreach (var i in ActionMenuDriver.field_Public_Static_ActionMenuDriver_0.field_Public_ActionMenuOpener_0.field_Public_ActionMenu_0.field_Public_List_1_ObjectNPublicPaSiAcObUnique_0) i.field_Public_AvatarPlayableController_0 = CurrentController;
-            foreach (var i in ActionMenuDriver.field_Public_Static_ActionMenuDriver_0.field_Public_ActionMenuOpener_1.field_Public_ActionMenu_0.field_Public_List_1_ObjectNPublicPaSiAcObUnique_0) i.field_Public_AvatarPlayableController_0 = CurrentController;
+            actionMenu = ActionMenuDriver.field_Public_Static_ActionMenuDriver_0;
+            EnableDisableListener listener = GameObject.Find("UserInterface/MenuContent/Screens/Avatar").AddComponent<EnableDisableListener>();
+            listener.OnEnabled += delegate { actionMenu.gameObject.SetActive(true); };
+            listener.OnDisabled += delegate { if (Patch) OnMenuClose(); };
+        }
+        
+        private static void HarmonyPatches()
+        {
+            Instance.Harmony.Patch(typeof(ActionMenuDriver).GetMethod(nameof(ActionMenuDriver.Method_Public_AvatarPlayableController_0)), new HarmonyMethod(typeof(Main).GetMethod(nameof(GetPlayableController))));
+            foreach (MethodInfo method in typeof(PageAvatar).GetMethods().Where(m => m.Name.Contains("Method_Private_Void_String_GameObject_AvatarPerformanceStats"))) Instance.Harmony.Patch(method, null, new HarmonyMethod(typeof(Main).GetMethod(nameof(OnPedestalAvatarInstantiated))));
         }
 
-        private static void SetPlayableController()
+        public static bool GetPlayableController(ref AvatarPlayableController __result)
         {
-            VRCAvatarDescriptor avatarDescriptor = GameObject.Find("UserInterface/MenuContent/Screens/Avatar/AvatarPreviewBase/MainRoot/MainModel/").GetComponentInChildren<VRCAvatarDescriptor>();
-            CurrentController = avatarDescriptor.gameObject.AddComponent(GameObject.Find($"{VRCPlayer.field_Internal_Static_VRCPlayer_0.gameObject.name}/AnimationController/PlayableController").GetComponent<AvatarPlayableController>());
-            CurrentController.field_Private_VRCPlayer_0 = VRCPlayer.field_Internal_Static_VRCPlayer_0;
+            if (Patch)
+            {
+                __result = CurrentController;
+                return false;
+            }
+            else return true;
+        }
+
+        public static void OnPedestalAvatarInstantiated()
+        {
+            VRCAvatarDescriptor CurrentDescriptor = GameObject.Find("UserInterface/MenuContent/Screens/Avatar/AvatarPreviewBase/MainRoot/MainModel/").GetComponentInChildren<VRCAvatarDescriptor>();
+            if (CurrentDescriptor != null)
+            {
+                SetPlayableController(CurrentDescriptor);
+                actionMenu.field_Public_VRC_AvatarDescriptor_0 = CurrentDescriptor;
+                // reposition ActionMenu
+                Patch = true;
+            }
+        }
+
+        private static void OnMenuClose()
+        {
+            VRCAvatarDescriptor CurrentDescriptor = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponentInChildren<VRCAvatarDescriptor>();
+            SetPlayableController(CurrentDescriptor);
+            actionMenu.field_Public_VRC_AvatarDescriptor_0 = CurrentDescriptor;
+            // reposition ActionMenu
+            Patch = false;
+        }
+
+        private static void SetPlayableController(VRCAvatarDescriptor avatarDescriptor)
+        {
+            CurrentController = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponentInChildren<AvatarPlayableController>();
             CurrentController.Method_Public_Void_VRCAvatarDescriptor_String_Boolean_PDM_0(avatarDescriptor, null, true);
-
-            ActionMenuDriver actionMenu = ActionMenuDriver.field_Public_Static_ActionMenuDriver_0;
-            actionMenu.field_Public_VRC_AvatarDescriptor_0 = avatarDescriptor;
-            actionMenu.gameObject.SetActive(true);
-        }
-    }
-
-    // Got from https://answers.unity.com/questions/530178/how-to-get-a-component-from-an-object-and-add-it-t.html
-    public static class Extention
-    {
-        public static T GetCopyOf<T>(this Component comp, T other) where T : Component
-        {
-            System.Type type = comp.GetType();
-            if (type != other.GetType()) return null; // type mis-match
-            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
-            PropertyInfo[] pinfos = type.GetProperties(flags);
-            foreach (var pinfo in pinfos)
-            {
-                if (pinfo.CanWrite)
-                {
-                    try
-                    {
-                        pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
-                    }
-                    catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
-                }
-            }
-            FieldInfo[] finfos = type.GetFields(flags);
-            foreach (var finfo in finfos)
-            {
-                finfo.SetValue(comp, finfo.GetValue(other));
-            }
-            return comp as T;
-        }
-
-        public static T AddComponent<T>(this GameObject go, T toAdd) where T : Component
-        {
-            return go.AddComponent<T>().GetCopyOf(toAdd) as T;
         }
     }
 }
