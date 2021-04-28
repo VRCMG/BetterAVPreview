61d315af456cb82a2578ce55a6ea9beec91809be
Davi <48662592+d-mageek@users.noreply.github.com>
2021-04-28 12:52:01 -0300

    First "usable" release maybe?
    Implementation of temporary rotator until Louky finishes his, plus some organisation
    
    also last commit was supposed to be Action Menu, not Quick lol
    
    to do:
    - fix bug that when u use the av preview ur avatar expressions wont work (local)
    - fix bug that the actionmenu keeps opened on the expressions page, leading to it not updating until closing and opening the page again
    - see if fixing the shaders glitch is possible
    - implement pop-up to better preview
    - wait for louky to finish rotator to use his instead of my current one
    - see if it's possible to use animations
    done:
    - facial expressions
    - changing states like clothes and colors
    - simple rotation mechanism with sliders
    - integration with ActionMenu

--- AVPreview3.0/Main.cs
+++ AVPreview3.0/Main.cs
@@ -1,11 +1,7 @@
-﻿using System.Linq;
-using System.Reflection;
-using UnhollowerRuntimeLib;
+﻿using UnhollowerRuntimeLib;
 using Harmony;
 using MelonLoader;
 using UnityEngine;
-using VRC.SDK3.Avatars.Components;
-using VRC.UI;
 using AVPreview.Utils;
 
 namespace AVPreview
@@ -20,66 +16,27 @@ namespace AVPreview
     public class Main : MelonMod
     {
         private static MelonMod Instance;
-        private static ActionMenuDriver actionMenu;
-        private static AvatarPlayableController CurrentController;
-        private static bool Patch;
+        public static HarmonyInstance HarmonyInstance => Instance.Harmony;
+        public static EnableDisableListener listener;
 
         public override void OnApplicationStart()
         {
             Instance = this;
             ClassInjector.RegisterTypeInIl2Cpp<EnableDisableListener>();
             HarmonyPatches();
+            MelonLogger.Msg("Successfully loaded!");
         }
 
         public override void VRChat_OnUiManagerInit()
         {
-            actionMenu = ActionMenuDriver.field_Public_Static_ActionMenuDriver_0;
-            EnableDisableListener listener = GameObject.Find("UserInterface/MenuContent/Screens/Avatar").AddComponent<EnableDisableListener>();
-            listener.OnEnabled += delegate { actionMenu.gameObject.SetActive(true); };
-            listener.OnDisabled += delegate { if (Patch) OnMenuClose(); };
+            listener = GameObject.Find("UserInterface/MenuContent/Screens/Avatar").AddComponent<EnableDisableListener>();
+            Controller.VRChat_OnUiManagerInit();
+            Rotator.VRChat_OnUiManagerInit();
         }
-        
+
         private static void HarmonyPatches()
         {
-            Instance.Harmony.Patch(typeof(ActionMenuDriver).GetMethod(nameof(ActionMenuDriver.Method_Public_AvatarPlayableController_0)), new HarmonyMethod(typeof(Main).GetMethod(nameof(GetPlayableController))));
-            foreach (MethodInfo method in typeof(PageAvatar).GetMethods().Where(m => m.Name.Contains("Method_Private_Void_String_GameObject_AvatarPerformanceStats"))) Instance.Harmony.Patch(method, null, new HarmonyMethod(typeof(Main).GetMethod(nameof(OnPedestalAvatarInstantiated))));
-        }
-
-        public static bool GetPlayableController(ref AvatarPlayableController __result)
-        {
-            if (Patch)
-            {
-                __result = CurrentController;
-                return false;
-            }
-            else return true;
-        }
-
-        public static void OnPedestalAvatarInstantiated()
-        {
-            VRCAvatarDescriptor CurrentDescriptor = GameObject.Find("UserInterface/MenuContent/Screens/Avatar/AvatarPreviewBase/MainRoot/MainModel/").GetComponentInChildren<VRCAvatarDescriptor>();
-            if (CurrentDescriptor != null)
-            {
-                SetPlayableController(CurrentDescriptor);
-                actionMenu.field_Public_VRC_AvatarDescriptor_0 = CurrentDescriptor;
-                // reposition ActionMenu
-                Patch = true;
-            }
-        }
-
-        private static void OnMenuClose()
-        {
-            VRCAvatarDescriptor CurrentDescriptor = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponentInChildren<VRCAvatarDescriptor>();
-            SetPlayableController(CurrentDescriptor);
-            actionMenu.field_Public_VRC_AvatarDescriptor_0 = CurrentDescriptor;
-            // reposition ActionMenu
-            Patch = false;
-        }
-
-        private static void SetPlayableController(VRCAvatarDescriptor avatarDescriptor)
-        {
-            CurrentController = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponentInChildren<AvatarPlayableController>();
-            CurrentController.Method_Public_Void_VRCAvatarDescriptor_String_Boolean_PDM_0(avatarDescriptor, null, true);
+            Controller.HarmonyPatches();
         }
     }
 }
---
+++ AVPreview3.0/Rotator.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using MelonLoader;
+
+namespace AVPreview
+{
+    public class Rotator
+    {
+        private static Transform Avatar;
+        private static Transform EmmButton;
+        private static Transform ChangeButton;
+        private static Transform FavButton;
+        private static Transform AvPreview;
+        private static Transform AvRoot;
+        private static Transform AvModel;
+        private static Quaternion InitRot;
+        private static GameObject sliderCloneX;
+        private static GameObject sliderCloneY;
+        private static GameObject sliderCloneTextX;
+        private static GameObject sliderCloneTextY;
+        private static Vector3 OriScale;
+        private static Vector3 proportion;
+        private static bool UsingEmm() => MelonHandler.Mods.Any(m => m.Info.Name.Contains("emmVRC"));
+        private static bool runOnce = false;
+
+        public static void VRChat_OnUiManagerInit()
+        {
+            foreach (MelonMod mod in MelonHandler.Mods) MelonLogger.Msg($"{mod.ToString()}");
+            GameObject Menu = GameObject.Find("UserInterface/MenuContent/Screens");
+            Transform sliderObj = Menu.transform.Find("Settings/MousePanel/SensitivitySlider");
+            Avatar = Menu.transform.Find("Avatar");
+
+            FavButton = Avatar.Find("Favorite Button");
+            ChangeButton = Avatar.Find("Change Button");
+            AvPreview = Avatar.Find("AvatarPreviewBase");
+            AvRoot = AvPreview.Find("MainRoot");
+            AvModel = AvRoot.Find("MainModel");
+
+            GameObject toggleClone = Object.Instantiate(sliderObj.parent.Find("InvertedMouse").gameObject, Avatar.transform);
+            sliderCloneX = Object.Instantiate(sliderObj.gameObject, Avatar.transform);
+            sliderCloneTextX = Object.Instantiate(sliderObj.parent.Find("MouseSensitivityText").gameObject, sliderCloneX.transform);
+            sliderCloneTextX.transform.position = sliderCloneX.transform.position;
+            sliderCloneY = Object.Instantiate(sliderObj.gameObject, Avatar.transform);
+            sliderCloneTextY = Object.Instantiate(sliderObj.parent.Find("MouseSensitivityText").gameObject, sliderCloneY.transform);
+            sliderCloneTextY.transform.position = sliderCloneY.transform.position;
+
+            toggleClone.GetComponent<Toggle>().onValueChanged = new Toggle.ToggleEvent();
+            sliderCloneX.GetComponent<Slider>().onValueChanged = new Slider.SliderEvent();
+            sliderCloneY.GetComponent<Slider>().onValueChanged = new Slider.SliderEvent();
+            toggleClone.GetComponent<Toggle>().onValueChanged.AddListener((UnityEngine.Events.UnityAction<bool>)((isOn) => { OnButtonToggle(isOn); }));
+            sliderCloneX.GetComponent<Slider>().onValueChanged.AddListener((UnityEngine.Events.UnityAction<float>)((_) => { OnSlide(); }));
+            sliderCloneY.GetComponent<Slider>().onValueChanged.AddListener((UnityEngine.Events.UnityAction<float>)((_) => { OnSlide(); }));
+            sliderCloneX.GetComponent<Slider>().value = 0.5f;
+            sliderCloneY.GetComponent<Slider>().value = 0.5f;
+
+            toggleClone.transform.Find("Label").GetComponent<Text>().text = "Toggle Manual Rotation";
+            sliderCloneTextX.GetComponent<Text>().text = "X:";
+            sliderCloneTextY.GetComponent<Text>().text = "Y:";
+            toggleClone.name = "ToggleRot";
+            sliderCloneX.name = "RotationX";
+            sliderCloneY.name = "RotationY";
+
+            toggleClone.GetComponent<Toggle>().isOn = false;
+            sliderCloneX.SetActive(false);
+            sliderCloneY.SetActive(false);
+
+            sliderCloneX.transform.position = ChangeButton.position;
+            sliderCloneY.transform.position = ChangeButton.position;
+        }
+
+        private static void OnButtonToggle(bool isOn)
+        {
+            if (isOn)
+            {
+                proportion = (FavButton.position - ChangeButton.position) * 4 / 13;
+                if (!runOnce)
+                {
+                    if (UsingEmm()) EmmButton = (from Button in Object.FindObjectsOfType<Button>() where Button.name == "Favorite Button(Clone)" && Button.gameObject.active == true select Button).First().transform;
+                    sliderCloneTextX.transform.position -= Quaternion.Euler(0, 0, -90) * proportion * 5 + proportion / 2;
+                    sliderCloneTextY.transform.position -= Quaternion.Euler(0, 0, -90) * proportion * 5 + proportion / 2;
+                    sliderCloneX.transform.position -= proportion * 4;
+                    sliderCloneY.transform.position -= proportion * 5 / 2;
+                    runOnce = true;
+                }
+                if (UsingEmm())
+                {
+                    EmmButton.position += proportion * 3;
+                    ChangeButton.position += proportion * 2;
+                    FavButton.position += proportion;
+                }
+                else
+                {
+                    OriScale = AvPreview.localScale;
+                    AvPreview.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+                    AvPreview.position -= proportion * 2;
+                    AvPreview.Find("FallbackRoot").position -= proportion;
+                    AvPreview.Find("EditPlayerDirLight").position -= proportion;
+                    Avatar.Find("Select Button").position -= proportion * 5 / 2;
+                }
+                AvModel.GetComponents<MonoBehaviour>().First(c => c.GetIl2CppType().FullName == "UnityStandardAssets.Utility.AutoMoveAndRotate").enabled = false;
+                AvRoot.position -= proportion * 10;
+                AvModel.position += proportion * 10;
+                InitRot = AvRoot.rotation;
+                AvRoot.rotation = new Quaternion(0, 0, 0, 0);
+                sliderCloneX.SetActive(true);
+                sliderCloneY.SetActive(true);
+            }
+            else
+            {
+                if (UsingEmm())
+                {
+                    EmmButton.position -= proportion * 3;
+                    ChangeButton.position -= proportion * 2;
+                    FavButton.position -= proportion;
+                }
+                else
+                {
+                    Avatar.Find("Select Button").position += proportion * 5 / 2;
+                    AvPreview.Find("EditPlayerDirLight").position += proportion;
+                    AvPreview.Find("FallbackRoot").position += proportion;
+                    AvPreview.position += proportion * 2;
+                    AvPreview.localScale = OriScale;
+                }
+                sliderCloneX.GetComponent<Slider>().value = 0.5f;
+                sliderCloneY.GetComponent<Slider>().value = 0.5f;
+                AvRoot.rotation = InitRot;
+                AvModel.position -= proportion * 10;
+                AvRoot.position += proportion * 10;
+                AvModel.GetComponents<MonoBehaviour>().First(c => c.GetIl2CppType().FullName == "UnityStandardAssets.Utility.AutoMoveAndRotate").enabled = true;
+                sliderCloneX.SetActive(false);
+                sliderCloneY.SetActive(false);
+            }
+        }
+
+        private static void OnSlide()
+        {
+            AvRoot.rotation = new Quaternion(0, 0, 0, 0);
+            AvModel.rotation = Quaternion.Euler(0, 360 * sliderCloneY.GetComponent<Slider>().value - 180, 0);
+            AvRoot.RotateAround(AvRoot.position, Vector3.right, 360 * sliderCloneX.GetComponent<Slider>().value - 180);
+        }
+    }
+}
